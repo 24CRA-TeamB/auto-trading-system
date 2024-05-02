249f2c2f2ef39a27768eb0688a854c01978094be
WonCheolHwang <wch18735@naver.com>
2024-05-02 14:28:31 +0900

    FIX CONFLICT

--- src/main/java/domain/Stock.java
+++ src/main/java/domain/Stock.java
@@ -1,5 +1,34 @@
 package domain;
 
 public class Stock {
-    
+    private final String stockCode;
+    private int stockPrice;
+
+    public Stock(String stockCode) {
+        this.stockCode = stockCode;
+    }
+
+    public Stock(String stockCode, int stockPrice) {
+
+        checkIllegalArgument(stockCode, stockPrice);
+
+        this.stockCode = stockCode;
+        this.stockPrice = stockPrice;
+    }
+
+    public String getStockCode() {
+        return this.stockCode;
+    }
+
+    public int getStockPrice() {
+        return stockPrice;
+    }
+
+    private void checkIllegalArgument(String stockCode, int stockPrice) {
+        if(stockCode == null || stockCode.equals(""))
+            throw new IllegalArgumentException("Stock Code가 비어있음.");
+
+        if(stockPrice < 0)
+            throw new IllegalArgumentException("Stock Price는 음수로 설정할 수 없음.");
+    }
 }
--- src/main/java/driver/MockDriver.java
+++ src/main/java/driver/MockDriver.java
@@ -18,21 +18,6 @@ public class MockDriver implements DriverInterface {
         stockPriceMap.put(STOCK_CODE_AAA, new Stock(STOCK_CODE_AAA, INITIAL_PRICE_AAA));
     }
 
-    @Override
-    public void login(String id, String password) {
-
-    }
-
-    @Override
-    public void buy(String stockCode, int amount, int price) {
-
-    }
-
-    @Override
-    public void sell(String stockCode, int amount, int price) {
-
-    }
-
     public void login(String id, String password) {
         if(id==null || id.equals(""))
             throw new IllegalArgumentException("ID가 비어있음");
@@ -58,7 +43,7 @@ public class MockDriver implements DriverInterface {
         if (stockPriceMap.containsKey(stockCode) == false) {
             return 0;
         } else {
-            return stockPriceMap.get(stockCode).getPrice();
+            return stockPriceMap.get(stockCode).getStockPrice();
         }
     }
 }
