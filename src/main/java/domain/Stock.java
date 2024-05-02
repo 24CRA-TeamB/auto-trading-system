2e7fba85af5672d9eebc9d0550b26b56fdbd6674
jiseongyoo <jsing8@nate.com>
2024-05-02 14:27:39 +0900

    Stock class uses getter/setter method

--- src/main/java/domain/Stock.java
+++ src/main/java/domain/Stock.java
@@ -1,7 +1,7 @@
 package domain;
 
 public class Stock {
-    private final String stockCode;
+    private String stockCode;
     private int stockPrice;
       
     public Stock(String stockCode) {
@@ -9,26 +9,29 @@ public class Stock {
     }
       
     public Stock(String stockCode, int stockPrice) {
-
-        checkIllegalArgument(stockCode, stockPrice);
-
-        this.stockCode = stockCode;
-        this.stockPrice = stockPrice;
+        setStockCode(stockCode);
+        setStockPrice(stockPrice);
     }
       
     public String getStockCode() {
         return this.stockCode;
     }
 
+    private void setStockCode(String stockCode) {
+        if (stockCode == null || stockCode.isEmpty()) {
+            throw new IllegalArgumentException("Stock Code가 비어있음.");
+        }
+        this.stockCode = stockCode;
+    }
+
     public int getStockPrice() {
         return stockPrice;
     }
-  
-    private void checkIllegalArgument(String stockCode, int stockPrice) {
-        if(stockCode == null || stockCode.equals(""))
-            throw new IllegalArgumentException("Stock Code가 비어있음.");
 
-        if(stockPrice < 0)
+    private void setStockPrice(int stockPrice) {
+        if(stockPrice <= 0) {
             throw new IllegalArgumentException("Stock Price는 음수로 설정할 수 없음.");
+        }
+        this.stockPrice = stockPrice;
     }
 }
